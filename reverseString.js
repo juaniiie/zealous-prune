6e873b9b26e8eb14836b3f1af25bb622c09d11f5
juaniiie <becjuana@gmail.com>
2015-12-02 15:40:43 -0800

    Fix: reverse1 is now working Feature: Adds reverse2 function

--- reverseString.js
+++ reverseString.js
@@ -4,8 +4,12 @@ What are three ways to reverse a string?
 
 var reverse1 = function(str) {
     var newString = '';
-    for (var i = 0; i < str.length; i++) {
+    for (var i = str.length - 1; i >= 0; i--) {
         newString += str[i];
     }
     return newString;
+};
+
+var reverse2 = function(str) {
+    return str.split('').reverse().join('');
 };
