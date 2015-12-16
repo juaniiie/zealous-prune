f04198445fda703c58b20bc3139d39002a2514de
juaniiie <becjuana@gmail.com>
2015-12-15 22:44:03 -0800

    Feature: Adds findPathsNxNBoard.js file

---
+++ findPathsNxNBoard.js
@@ -0,0 +1,13 @@
+//given a board n X n
+//find number of possible paths 
+//one can take from top left to bottom right
+
+var board = [
+[0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0]
+];
+
+ 
