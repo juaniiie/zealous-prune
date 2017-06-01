018cad01644f27a436272e4dc9fedbbf6f2f719d
juaniiie <becjuana@gmail.com>
2017-06-01 16:49:06 -0700

    Add map to keep dependencies

--- ctci6/buildOrder.js
+++ ctci6/buildOrder.js
@@ -6,6 +6,32 @@
  * Output: ['f', 'e', 'a', 'b', 'd', 'c']
  */
 
-function findBuildOrder(projects, dependencies) {
-    
+function findDependency(list, item) {
+    for (let d = 0; d < list.lenght; d++) {
+        if (d[0] === item) {
+            return d[1];
+        }
+    }
 }
+
+function findBuildOrder(projects, dependencies) {
+    let depend = new Map();
+
+    for (let i = 0; i < dependencies.length; i++) {
+        let duple = dependencies[i];
+        if (!depend.has(duple[0])) {
+            depend.set(duple[0], [duple[1]]);
+        } else if (depend.has(duple[0])){
+            let entry = depend.get(duple[0]);
+            entry.push(duple[1]);
+            depend.set(duple[0], entry);
+        }
+    }
+
+    // console.log('depend entries', depend.entries());
+
+}
+
+const pro = ['a', 'b', 'c', 'd', 'e', 'f'];
+const de = [['d', 'a'], ['b', 'f'], ['d', 'b'], ['a', 'f'], ['c', 'd']];
+findBuildOrder(pro, de);
