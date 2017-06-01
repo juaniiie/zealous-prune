2a1c551296348165f6276ac2ea78e821499d8a37
juaniiie <becjuana@gmail.com>
2017-06-01 14:44:08 -0700

    Adds build order

---
+++ ctci6/buildOrder.js
@@ -0,0 +1,11 @@
+/**
+ * Input
+ * projects: ['a', 'b', 'c', 'd', 'e', 'f']
+ * dependencies: [['d', 'a'], ['b', 'f'], ['d', 'b'], ['a', 'f'], ['c', 'd']]
+ * 
+ * Output: ['f', 'e', 'a', 'b', 'd', 'c']
+ */
+
+function findBuildOrder(projects, dependencies) {
+    
+}
