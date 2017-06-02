941b4f7a25c8e3382af83c661e876b929ed25f4f
juaniiie <becjuana@gmail.com>
2017-06-02 01:00:33 -0700

    Tentative solution

--- ctci6/buildOrder.js
+++ ctci6/buildOrder.js
@@ -5,34 +5,6 @@
  * 
  * Output: ['f', 'e', 'a', 'b', 'd', 'c']
  */
-// let reqs = [];
-function findReqDependencies(item, map, reqs) {
-    let itemDependencies = map.get(item);
-
-    if (!itemDependencies) {
-        // console.log('dependencies is null', item);
-        if (reqs.indexOf(item) === -1) {
-            // console.log('unshifing', item);
-            reqs.unshift(item);
-            // console.log('resulting resp', reqs);
-        }
-        return;
-    }
-    
-    for (var i = 0; i < itemDependencies.length; i++) {
-        let dependency = itemDependencies[i];
-        findReqDependencies(itemDependencies[i], map);
-        if (reqs.indexOf(dependency) === -1 && itemDependencies !== null) {
-            reqs.push(dependency);
-        }
-    }
-
-    if (reqs.indexOf(item) === -1) {
-        reqs.push(item);
-    }
-
-    return reqs;
-};
 
 /**
  * Solution
@@ -53,12 +25,36 @@ function findBuildOrder(projects, dependencies) {
         }
     }
 
-    let r = [];
+    let reqs = [];
+    function findReqDependencies(item, map) {
+        let itemDependencies = map.get(item);
+
+        if (!itemDependencies) {
+            if (reqs.indexOf(item) === -1) {
+                reqs.unshift(item);
+            }
+            return;
+        }
+        
+        for (var i = 0; i < itemDependencies.length; i++) {
+            let dependency = itemDependencies[i];
+            findReqDependencies(itemDependencies[i], map);
+            if (reqs.indexOf(dependency) === -1 && itemDependencies !== null) {
+                reqs.push(dependency);
+            }
+        }
+
+        if (reqs.indexOf(item) === -1) {
+            reqs.push(item);
+        }
+    };
+
     for (let j = 0; j < projects.length; j++) {
         let project = projects[j];
-        findReqDependencies(project, depend, r);
+        findReqDependencies(project, depend);
     }
-
+    
+    return reqs;
 }
 
 const pro = ['a', 'b', 'c', 'd', 'e', 'f'];
