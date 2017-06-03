d8c70fc3fed22bf1a0134e3673bb648e3ea5d6db
juaniiie <becjuana@gmail.com>
2017-06-02 18:33:09 -0700

    Trying topological approach

--- ctci6/buildOrder.js
+++ ctci6/buildOrder.js
@@ -10,64 +10,38 @@
  * Solution
  * 
  */
-function findBuildOrder(projects, dependencies) {
-    let depend = new Map();
-    let orderedList = [];
+function visit(visitedSet, project, result) {
 
-    for (let i = 0; i < dependencies.length; i++) {
-        let duple = dependencies[i];
-        if (!depend.has(duple[0])) {
-            depend.set(duple[0], [duple[1]]);
-        } else if (depend.has(duple[0])){
-            let entry = depend.get(duple[0]);
-            entry.push(duple[1]);
-            depend.set(duple[0], entry);
-        }
-    }
-    console.log('depend', depend.entries());
-
-    let reqs = [];
-    let originalItem;
-    function findReqDependencies(item, map) {
-        let itemDependencies = map.get(item);
-
-        if (!itemDependencies) {
-            if (reqs.indexOf(item) === -1) {
-                reqs.unshift(item);
-            }
-            return;
-        }
-        //2
-        for (var i = 0; i < itemDependencies.length; i++) {
-            let dependency = itemDependencies[i];
-            if (dependency === originalItem) {
-                throw new Error('dependencies are cyclical');
-            }
-            findReqDependencies(itemDependencies[i], map);
-            if (reqs.indexOf(dependency) === -1 && itemDependencies !== null) {
-                reqs.push(dependency);
-            }
-        }
-        //3
-        if (reqs.indexOf(item) === -1) {
-            reqs.push(item);
-        }
-    };
-
-    for (let j = 0; j < projects.length; j++) {
-        let project = projects[j];
-        originalItem = project;
-        findReqDependencies(project, depend);
-    }
-    
-    return reqs;
 }
 
-const pro = [9, 1, 5, 6];
-const de = [[6, 5], [5, 1], [1, 9], [9, 5]];
+function buildGraph(dependencies) {
+    let graph = new Map();
+
+    for (let i = 0; i < dependencies.length; i++) {
+        let node = dependencies[i][1];
+        let edge = dependencies[i][0];
+
+        if (!graph.has(node)) {
+            graph.set(node, [edge]);
+        } else if (graph.has(node)) {
+            let edges = graph.get(node);
+            edges.push(edge);
+            graph.set(node, edges);
+        }
+    }
+    return graph;
+}
+
+function findBuildOrder(projects, dependencies) {
+    let visited = new Set();
+    let result = [];
+    let graph = buildGraph(dependencies);
+    
+
+    console.log('graph entries', graph.entries());
+
+}
+
+const pro = ['a', 'b', 'c', 'd', 'e', 'f'];
+const de = [['d', 'a'], ['b', 'f'], ['d', 'b'], ['a', 'f'], ['c', 'd']];
 findBuildOrder(pro, de);
-
-// add dependency
-// check dependency's dependencies
-// add those
-
