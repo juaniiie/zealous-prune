8b5590d6575d40d89a42971ab395ec7e8d7f2108
juaniiie <becjuana@gmail.com>
2017-06-04 16:17:11 -0700

    Adds scaffolding

---
+++ ctci6/findFirstCommonAnscestor.js
@@ -0,0 +1,22 @@
+/**
+ * Solution if access to parent
+ */
+function findFirstCommonAnscestor(node1, node2) {
+
+}
+
+function findDepthFromBottom(node) {
+    let depthLeft = 0;
+    let depthRight = 0;
+
+
+
+}
+
+function dfs(node, depth) {
+    if (!node) {
+        return;
+    }
+
+    
+}
