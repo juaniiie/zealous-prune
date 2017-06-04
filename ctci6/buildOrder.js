f024aecc6462a29117f2f611d35b29eb7da0f5e0
juaniiie <becjuana@gmail.com>
2017-06-03 21:40:58 -0700

    Add complete solution

--- ctci6/buildOrder.js
+++ ctci6/buildOrder.js
@@ -14,17 +14,29 @@ function visit(project, visited, result, graph) {
     if (result.indexOf(project) > -1) {
         return;
     }
-    if (visited.has(project) || !graph.has(project)) {
-        result.unshift(project);
+    if (visited.has(project)) {
+        throw new Error('dependencies are cyclic');
+    }
+
+    if (!graph.has(project)) {
         visited.add(project);
+        result.unshift(project);
         return;
     }
+
     if (!visited.has(project)) {
         visited.add(project);
     }
+    
     let edges = graph.get(project);
+    
     for (let i = 0; i < edges.length; i++) {
-        visit(edges[i], visited, result, graph);
+      visit(edges[i], visited, result, graph);
+    }
+    
+    if (result.indexOf(project) === -1) {
+      result.unshift(project);
+      return;
     }
 }
 
@@ -50,18 +62,31 @@ function findBuildOrder(projects, dependencies) {
     let visited = new Set();
     let result = [];
     let graph = buildGraph(dependencies);
-
-
-    // console.log('graph entries', graph.entries());
     
-    for (let i = 0; projects.length; i++) {
+    for (let i = 0; i < projects.length; i++) {
         visit(projects[i], visited, result, graph);
     }
-
-    console.log('result');
+    console.log('result', result);
+    return result;
 
 }
 
-const pro = ['a', 'b', 'c', 'd', 'e', 'f'];
-const de = [['d', 'a'], ['b', 'f'], ['d', 'b'], ['a', 'f'], ['c', 'd']];
+const pro = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+const de = [
+        [2, 1],
+        [3, 1],
+        [4, 2],
+        [6, 2],
+        [5, 3],
+        [7, 3],
+        [8, 4],
+        [11, 8],
+        [12, 8],
+        [10, 6],
+        [10, 5],
+        [9, 7],
+        [13, 10],
+        [13, 9],
+        [14, 13]
+      ];
 findBuildOrder(pro, de);
