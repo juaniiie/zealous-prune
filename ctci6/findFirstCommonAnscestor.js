ad04c1e60a66a132cb37da65606b6696f5b85993
juaniiie <becjuana@gmail.com>
2017-06-14 20:29:13 -0700

    Adds solution to findFirstCommonAncestor

--- ctci6/findFirstCommonAnscestor.js
+++ ctci6/findFirstCommonAnscestor.js
@@ -33,27 +33,51 @@ class Tree {
 
 /**
  * Solution if access to parent
+ * Steps:
+ * 1. Find distance from bottom of tree for both nodes
+ * 2. If different, advance lower node to same level as higher node
+ * 3. Go up the tree (to each node's parent), to find where they equal
+ * 
+ * Time: O(lg N) - assumes balanced tree, worst case O(N) 
+ * Space: O(1) 
  */
 function findFirstCommonAnscestor(node1, node2) {
     if (!node1 || !node2) {
         throw new Error('must be valid nodes');
     }
     if (node1 === node2) {
-        return node1;
+      // console.log('parent at check: ', node1.val);
+        return node1.val;
     }
 
     let node1Depth = findDepthFromBottom(0, node1);
     let node2Depth = findDepthFromBottom(0, node2);
 
-    console.log('cache1', node1Depth);
-    console.log('cache2', node2Depth);
+    if (node1Depth > node2Depth) {
+      for (var i = 0; i < node1Depth - node2Depth; i++) {
+        node2 = node2.parent;
+      }
+    } else if (node1Depth < node2Depth) {
+      for (var j = 0; j < node2Depth - node1Depth; j++) {
+        node1 = node1.parent;
+      }
+    }
 
+    let parent;
+    while (parent === undefined) {
+      if (node1 === node2) {
+        parent = node1;
+      }
+      node1 = node1.parent;
+      node2 = node2.parent;
+
+    }
+ 
+    return parent.val; 
 }
 
 
 function findDepthFromBottom(depth, node) {
-  console.log('node', node ? node.val : null);
-  console.log('depth', depth);
   if (!node) {
     return depth;
   } else {
@@ -61,12 +85,14 @@ function findDepthFromBottom(depth, node) {
   }
 }
 
+// Test cases
 
 let tree = new Tree();
 
-[8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15].forEach(v => tree.add(v));
+// [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15].forEach(v => tree.add(v));
+[10, 9, 11].forEach(v => tree.add(v));
 
-let nodeA = tree.root.left.left.left; //1
-let nodeB = tree.root.left.right; //6
+let nodeA = tree.root.left;
+let nodeB = tree.root;
 
 findFirstCommonAnscestor(nodeA, nodeB);
