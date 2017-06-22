df71dc34b1b1c1c1481ef36dbe79909d8dc842ba
juaniiie <becjuana@gmail.com>
2017-06-21 20:37:18 -0700

    Adds leet solution

---
+++ leet/shortest-word-distance.js
@@ -0,0 +1,47 @@
+/**
+ * Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+ * 
+ * For example,
+ * Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+ * 
+ * Given word1 = “coding”, word2 = “practice”, return 3.
+ * Given word1 = "makes", word2 = "coding", return 1.
+ * 
+ * Note:
+ * You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+ */
+
+
+/**
+ * @param {string[]} words
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var shortestDistance = function(words, word1, word2) {
+    let minDistace = words.length;
+    let indexes = {};
+    indexes[word1] = [];
+    indexes[word2] = [];
+
+    function updateMin(value, wordKey) {
+        let list = indexes[wordKey];
+        list.forEach((int) => {
+            minDistace = Math.min(minDistace, Math.abs(value - int));
+        });
+    }
+
+    words.forEach((word, i) => {
+        if (word === word1 || word === word2) {
+            indexes[word].push(i);
+            let target = word1 === word ? word2 : word1;
+            updateMin(i, target);
+        }
+    }); 
+
+    return minDistace; 
+};
+
+let testList = ['a', 'b'];
+
+console.log(shortestDistance(testList, 'a', 'b'));
